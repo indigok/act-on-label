d1d0da684ee9c8d470395b0c8dca46e21a6c54d1
hmharvey <hmharvey@github.com>
2019-11-04 23:30:00 -0800

    Fix env variable naming

--- lib/act_on_labels.rb
+++ lib/act_on_labels.rb
@@ -6,7 +6,6 @@ if ENV["GITHUB_EVENT_NAME"] == "pull_request"
   payload = JSON.parse(File.read(ENV["GITHUB_EVENT_PATH"]))
 
   if payload["action"] == "labeled" 
-    puts payload["label"]
-    exit(false) unless payload["label"]["name"] == ENV["LABELS"]
+    exit(false) unless payload["label"]["name"] == ENV["INPUT_LABELS"]
   end
 end
