b4317b39fe557c3e39753b7181a22f027d77da4a
hmharvey <hmharvey@github.com>
2019-11-04 23:14:09 -0800

    Fix hash key name

--- lib/act_on_labels.rb
+++ lib/act_on_labels.rb
@@ -6,7 +6,6 @@ if ENV["GITHUB_EVENT_NAME"] == "pull_request"
   payload = JSON.parse(File.read(ENV["GITHUB_EVENT_PATH"]))
 
   if payload["action"] == "labeled" 
-    puts payload.keys
-    exit(false) unless payload["labels"].any? { |label| label["name"] == ENV["LABELS"]} 
+    exit(false) unless payload["label"].any? { |label| label["name"] == ENV["LABELS"]} 
   end
 end
